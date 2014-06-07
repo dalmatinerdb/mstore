3c3ae8a8550b7364f54ae8a61059a314c84eb20e
Heinz N. Gies <heinz@licenser.net>
2014-06-07 19:44:34 +0200

    Multi put working with single values.

--- src/mstore.erl
+++ src/mstore.erl
@@ -26,7 +26,7 @@
 -spec open(Dir :: string()) -> {ok, #mset{}} | {error, not_found}.
 
 open(Dir) ->
-    case file:consult(Dir++"/mstore") of
+    case file:consult([Dir | "/mstore"]) of
         {ok, [{FileSize, NumFiles, Seed}]} ->
             {ok, #mset{size=FileSize, chash=chash:fresh(NumFiles, []), dir=Dir, seed=Seed}};
         _ ->
@@ -37,7 +37,7 @@ new(NumFiles, FileSize, Dir) when is_binary(Dir) ->
     new(NumFiles, FileSize, binary_to_list(Dir));
 
 new(NumFiles, FileSize, Dir) ->
-    case file:consult(Dir++"/mstore") of
+    case file:consult([Dir | "/mstore"]) of
         {ok, [{F, N, Seed}]} when F =:= FileSize,
                                   N =:= NumFiles ->
             {ok, #mset{size=FileSize, chash=chash:fresh(NumFiles, []), dir=Dir, seed=Seed}};
@@ -46,54 +46,85 @@ new(NumFiles, FileSize, Dir) ->
         _ ->
             Seed = erlang:phash2(now()),
             file:make_dir(Dir),
-            file:write_file(Dir++"/mstore",
+            file:write_file([Dir | "/mstore"],
                             io_lib:format("~p.", [{FileSize, NumFiles, Seed}])),
             CHash = {_, Idxs} = chash:fresh(NumFiles, []),
-            [file:make_dir([Dir, $/, integer_to_list(I)]) || {I, _} <- Idxs],
+            [file:make_dir([Dir, $/ | integer_to_list(I)]) || {I, _} <- Idxs],
             {ok, #mset{size=FileSize, chash=CHash, dir=Dir, seed=Seed}}
     end.
 
-put(MSet = #mset{size=S, chash=CHash, dir=D, seed=Seed}, Metric, Time, Value) ->
-    FileBase = (Time div S)*S,
+put(MSet, Metric, Time, Values)
+  when is_list(Values) ->
+    put(MSet, Metric, Time, << <<V:64/integer>> || V <- Values >>);
+
+put(MSet = #mset{size=S, chash=CHash, seed=Seed, dir=D}, Metric, Time, Value)
+  when is_binary(Value)
+       ->
     <<IndexAsInt:160/integer>> = chash:key_of({Seed, Metric}),
     Idx = chash:next_index(IndexAsInt, CHash),
-    case chash:lookup(Idx, CHash) of
-        [] ->
-            Base = [D, $/, integer_to_list(Idx), $/, integer_to_list(FileBase)],
-            {ok, F} = open(Base, FileBase, S),
-            {ok, F1} = write(F, Metric, Time, Value),
-            CHash1 = chash:update(Idx, [{FileBase, F1}], CHash),
-            MSet#mset{chash = CHash1};
-        [{FileBase, F} | R] ->
-            {ok, F1} = write(F, Metric, Time, Value),
-            CHash1 = chash:update(Idx, [{FileBase, F1}| R], CHash),
-            MSet#mset{chash = CHash1};
-        [First, {FileBase, F}] ->
-            {ok, F1} = write(F, Metric, Time, Value),
-            CHash1 = chash:update(Idx, [{FileBase, F1}, First], CHash),
-            MSet#mset{chash = CHash1};
-        [First] ->
-            Base = [D, $/, integer_to_list(Idx), $/, integer_to_list(FileBase)],
-            {ok, F1} = open(Base, FileBase, S),
-            {ok, F2} = write(F1, Metric, Time, Value),
-            CHash1 = chash:update(Idx, [{FileBase, F2}, First], CHash),
-            MSet#mset{chash = CHash1};
-        [First, {_Other, F}] ->
-            close(F),
-            Base = [D, $/, integer_to_list(Idx), $/, integer_to_list(FileBase)],
-            {ok, F1} = open(Base, FileBase, S),
-            {ok, F2} = write(F1, Metric, Time, Value),
-            CHash1 = chash:update(Idx, [{FileBase, F2}, First], CHash),
+    Count = byte_size(Value) / ?DATA_SIZE,
+    Parts = make_splits(Time, Count, S, []),
+    CurFiles = chash:lookup(Idx, CHash),
+    Parts1 = [{B, round(C*?DATA_SIZE)} || {B, C} <- Parts],
+    case do_put(MSet#mset{dir=[D, $/, integer_to_list(Idx)]}, Metric, Parts1, Value, CurFiles) of
+        CurFiles1 when CurFiles1 =:= CurFiles ->
+            MSet;
+        CurFiles1 ->
+            CHash1 = chash:update(Idx, CurFiles1, CHash),
             MSet#mset{chash = CHash1}
+    end;
 
-    end.
+
+put(MSet, Metric, Time, V) ->
+    put(MSet, Metric, Time, <<V:64/integer>>).
+
+do_put(_, _, [], <<>>, Files) ->
+    Files;
+
+do_put(MSet = #mset{size=S}, Metric,
+       [{Time, Size} | R], InData,
+       [{FileBase, F} | FileRest]) when
+      ((Time div S)*S) =:= FileBase ->
+    <<Data:Size/binary, DataRest/binary>> = InData,
+    {ok, F1} = write(F, Metric, Time, Data),
+    do_put(MSet, Metric, R, DataRest, [{FileBase, F1} | FileRest]);
+
+do_put(MSet = #mset{size=S}, Metric,
+       [{Time, Size} | R], InData,
+       [First, {FileBase, F}]) when
+      ((Time div S)*S) =:= FileBase ->
+    <<Data:Size/binary, DataRest/binary>> = InData,
+    {ok, F1} = write(F, Metric, Time, Data),
+    do_put(MSet, Metric, R, DataRest, [{FileBase, F1} | First]);
+
+do_put(MSet = #mset{size=S, dir=D}, Metric,
+       [{Time, Size} | R], InData,
+       [First, {_Other, F}]) ->
+    <<Data:Size/binary, DataRest/binary>> = InData,
+    FileBase = (Time div S)*S,
+    close(F),
+    Base = [D, $/, integer_to_list(FileBase)],
+    {ok, F1} = open(Base, FileBase, S),
+    {ok, F2} = write(F1, Metric, Time, Data),
+    do_put(MSet, Metric, R, DataRest, [{FileBase, F2} | First]);
+
+do_put(MSet = #mset{size=S, dir=D}, Metric,
+       [{Time, Size} | R], InData,
+       Files) ->
+    %%io:format("<<Data:~p/binary, DataRest/binary>> = ~p~n", [Size, InData]),
+    <<Data:Size/binary, DataRest/binary>> = InData,
+    FileBase = (Time div S)*S,
+    Base = [D, $/, integer_to_list(FileBase)],
+    {ok, F1} = open(Base, FileBase, S),
+    {ok, F2} = write(F1, Metric, Time, Data),
+    do_put(MSet, Metric, R, DataRest, [{FileBase, F2} | Files]).
 
 get(#mset{size=S, chash=CHash, dir=D, seed=Seed}, Metric, Time, Count) ->
     Parts = make_splits(Time, Count, S, []),
     io:format("~p~n", [Parts]),
     <<IndexAsInt:160/integer>> = chash:key_of({Seed, Metric}),
     Idx = chash:next_index(IndexAsInt, CHash),
-    Dir = D ++ "/" ++ integer_to_list(Idx),
+    Dir = [D, "/", integer_to_list(Idx)],
     do_get(S, Dir, Metric, Parts, <<>>).
 
 do_get(_, _, _, [], Acc) ->
@@ -101,7 +132,7 @@ do_get(_, _, _, [], Acc) ->
 
 do_get(S, Dir, Metric, [{Time, Count} | R], Acc) ->
     FileBase = (Time div S)*S,
-    Base = Dir ++ "/" ++ integer_to_list(FileBase),
+    Base = [Dir, "/", integer_to_list(FileBase)],
     {ok, F} = open(Base, FileBase, S),
     case read(F, Metric, Time, Count) of
         {ok, D} ->
@@ -133,10 +164,10 @@ open(File, Offset, Size) when is_binary(File) ->
     open(binary_to_list(File), Offset, Size);
 
 open(File, Offset, Size) ->
-    case file:consult(File++".idx") of
+    case file:consult([File | ".idx"]) of
         {ok, [{O, S, Idx}]} when Offset =:= O,
                                  Size =:= S ->
-            case file:open(File++".mstore", [read, write | ?OPTS]) of
+            case file:open([File | ".mstore"], [read, write | ?OPTS]) of
                 {ok, F} ->
                     Tree=gb_trees:from_orddict(Idx),
                     {ok, #mstore{index=Tree, name=File, file=F, offset=Offset,
@@ -151,7 +182,7 @@ open(File, Offset, Size) ->
         {ok, _} ->
             {error, bad_index};
         _E ->
-            case file:open(File++".mstore", [read, write | ?OPTS]) of
+            case file:open([File |".mstore"], [read, write | ?OPTS]) of
                 {ok, F} ->
                     M = #mstore{name=File, file=F, offset=Offset,
                                 size=Size, next=0},
@@ -268,7 +299,7 @@ to_list(Bin) ->
     [I || <<I:64/integer>> <= Bin].
 
 write_index(#mstore{name=F, index=I, offset=O, size=S}) ->
-    file:write_file(F ++ ".idx", io_lib:format("~p.", [{O, S, gb_trees:to_list(I)}])).
+    file:write_file([F | ".idx"], io_lib:format("~p.", [{O, S, gb_trees:to_list(I)}])).
 
 -ifdef(TEST).
 
