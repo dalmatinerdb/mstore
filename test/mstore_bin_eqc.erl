765a47205293417e29d5c44915becfdceca7f949
Heinz N. Gies <heinz@licenser.net>
2014-06-11 23:07:38 +0200

    trying to beat eqc_ci into submission.

--- test/mstore_bin_eqc.erl
+++ test/mstore_bin_eqc.erl
@@ -24,27 +24,31 @@ empty_prop() ->
     ?FORALL(Length, non_neg_int(),
             byte_size(mstore_bin:empty(Length)) == Length*?DATA_SIZE).
 
-emtpy_test() ->
-    ?assert(eqc:quickcheck(empty_prop())).
-
 l2b_b2l_prop() ->
     ?FORALL(List, i_or_f_list(),
             List == ?B2L(?L2B(List))).
 
-l2b_b2l_test() ->
-    ?assert(eqc:quickcheck(l2b_b2l_prop())).
-
 b2l_prop() ->
     ?FORALL({_, L, B}, i_or_f_array(),
             L == ?B2L(B)).
-
-b2l_test() ->
-    ?assert(eqc:quickcheck(b2l_prop())).
-
 find_type_prop() ->
     ?FORALL({T, B}, non_obvious_list(),
             T == mstore_bin:find_type(B)).
 
-find_type_test() ->
-    ?assert(eqc:quickcheck(find_type_prop())).
 
+out(P) ->
+   on_output(fun(S,F) -> io:format(user, S, F) end,P).
+
+run_test_() ->
+    Props = [
+             fun empty_prop/0,
+             fun b2l_prop/0,
+             fun find_type_prop/0,
+             fun l2b_b2l_prop/0
+             ],
+    [
+     begin
+         P = out(Prop()),
+         ?_assert(quickcheck(numtests(500,P)))
+     end
+     || Prop <- Props].
