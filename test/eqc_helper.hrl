6dc43ebfe613a1ae8e2b851757f02f58082a341d
Heinz N. Gies <heinz@licenser.net>
2014-06-13 19:55:49 +0200

    improved eqc helper.

--- test/eqc_helper.hrl
+++ test/eqc_helper.hrl
@@ -1,28 +1,3 @@
--ifdef(EQC_CI).
--define(OUT(P),  on_output(fun(S,F) -> io:fwrite(user, S, F) end, P)).
--else.
--define(OUT(P),
-        on_output(fun
-                      (".", []) ->
-                          io:fwrite(user, <<"\e[0;32m*\e[0m">>, []);
-                     ("x", []) ->
-                          io:format(user, <<"\e[0;33mx\e[0m">>, []);
-                     ("Failed! ", []) ->
-                          io:format(user, <<"\e[0;31mFailed! \e[0m">>, []);
-                     (S, F) ->
-                          io:format(user, S, F)
-                  end, P)).
--endif.
-
-run_test_() ->
-    [{exports, E} | _] = module_info(),
-    E1 = [{atom_to_list(N), N} || {N, 0} <- E],
-    E2 = [{N, A} || {"prop_" ++ N, A} <- E1],
-    [{"Running " ++ N ++ " propperty test",
-      {timeout, 60, ?_assert(quickcheck(numtests(500,  ?OUT(?MODULE:A()))))}}
-     || {N, A} <- E2].
-
-
 %% Include this file at the END of _eqc.erl file!
 %%
 %% Allows running EQC in both EQC-CI and EQC offline. Placing eqc files in the
@@ -41,3 +16,39 @@ run_test_() ->
 %%
 %%{build, "make eqc-ci"}.
 %%{test_path, "."}.
+%%
+%% EQC_NUM_TESTS and EQC_EUNIT_TIMEUT can be -defined in the file including this
+%% if not they'll defualt to 500 tests and 60s
+
+
+-ifdef(EQC_CI).
+-define(OUT(P),  on_output(fun(S,F) -> io:fwrite(user, S, F) end, P)).
+-else.
+-define(OUT(P),
+        on_output(fun
+                      (".", []) ->
+                          io:fwrite(user, <<"\e[0;32m*\e[0m">>, []);
+                     ("x", []) ->
+                          io:format(user, <<"\e[0;33mx\e[0m">>, []);
+                     ("Failed! ", []) ->
+                          io:format(user, <<"\e[0;31mFailed! \e[0m">>, []);
+                     (S, F) ->
+                          io:format(user, S, F)
+                  end, P)).
+-endif.
+
+-ifndef(EQC_NUM_TESTS).
+-define(EQC_NUM_TESTS, 500).
+-endif.
+
+-ifndef(EQC_EUNIT_TIMEUT).
+-define(EQC_EUNIT_TIMEUT, 60).
+-endif.
+
+run_test_() ->
+    [{exports, E} | _] = module_info(),
+    E1 = [{atom_to_list(N), N} || {N, 0} <- E],
+    E2 = [{N, A} || {"prop_" ++ N, A} <- E1],
+    [{"Running " ++ N ++ " propperty test",
+      {timeout, ?EQC_EUNIT_TIMEUT, ?_assert(quickcheck(numtests(?EQC_NUM_TESTS,  ?OUT(?MODULE:A()))))}}
+     || {N, A} <- E2].
