e4786a9ef30cdbe00238a087d0a4b3d9fda488c3
Heinz N. Gies <heinz@licenser.net>
2016-12-05 15:56:04 +0100

    Add empty file

---
+++ src/mfile.erl
@@ -0,0 +1,10 @@
+%%%-------------------------------------------------------------------
+%%% @author Heinz N. Gies <heinz@licenser.net>
+%%% @copyright (C) 2016, Heinz N. Gies
+%%% @doc MStore file module. 
+%%%
+%%% @end
+%%% Created :  5 Dec 2016 by Heinz N. Gies <heinz@licenser.net>
+%%%-------------------------------------------------------------------
+-module(mfile).
+
